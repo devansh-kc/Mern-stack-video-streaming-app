3d5acfc5af84df2167a70b321205769dda68e2b9
devansh-kc <devansh.kc96@gmail.com>
2023-11-13 05:17:51 +0530

    connected to mongo db

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+// dotenv.config({
+//   path: "./env",
+// });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//   try {
+//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("Error", (error) => {
+//       console.log("ERROR :", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`APP is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR :", error);
+//     throw error;
+//   }
+// })();
