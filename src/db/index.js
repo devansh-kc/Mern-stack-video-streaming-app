aeba8b6b4505284972e67dfae54862bdd495b4d4
devansh-kc <devansh.kc96@gmail.com>
2023-11-15 16:21:33 +0530

    added user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,5 @@
-import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-
+import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
