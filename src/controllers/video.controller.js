24f5b0b8874cb1402dae162259cb3cf85cf718d5
devansh-kc <devansh.kc96@gmail.com>
2024-01-11 18:38:17 +0530

    worked on video controller to  upload the video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { User } from "../models/user.model.js";
-import { Video } from "../models/video.model.js";
+import { Video, Video } from "../models/video.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { APIError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
@@ -13,7 +13,43 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new APIError(400, "All fields are required");
+  }
+
   // TODO: get video, upload to cloudinary, create video
+
+  // TODO: Uploading Video
+  const videoFileLocalPath = req.files?.videoFile[0]?.path;
+  if (!videoFileLocalPath) {
+    throw new APIError(400, "Avatar Local file is required");
+  }
+  const videoURL = await uploadOnCloudinary(videoFileLocalPath);
+  if (!videoURL) {
+    throw new APIError(400, "Video Local file is required");
+  }
+  // TODO: Uploading thumbnail
+  const ThumbnailFileLocalPath = req.files?.thumbnail[0]?.path;
+  if (!ThumbnailFileLocalPath) {
+    throw new APIError(400, "Avatar Local file is required");
+  }
+  const thumbnailURL = await uploadOnCloudinary(ThumbnailFileLocalPath);
+  if (!thumbnailURL) {
+    throw new APIError(400, "Thumbnail Local file is required");
+  }
+  const Video = await Video.create({
+    title,
+    description,
+    videoFile: videoURL.url,
+    thumbnail: thumbnailURL.url,
+    duration: videoURL.duration(),
+    views: 0,
+    isPublished: true,
+    owner: req.user._id,
+  });
+  if (!Video) {
+    throw new APIError(500, "error while  uploading the video");
+  }
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
