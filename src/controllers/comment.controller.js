cad6d18d04d6aded29cb780939f28129c8306f2a
devansh-kc <devansh.kc96@gmail.com>
2024-03-24 02:20:10 +0530

    worked on update and delete comment functionality

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
@@ -9,12 +9,8 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
-  const video = await Video.findById(videoId);
-  if (!video) {
-    throw new APIError(404, "Video not found");
-  }
-
   
+
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -24,14 +20,17 @@ const addComment = asyncHandler(async (req, res) => {
   const { content } = req.body;
 
   try {
+    if(!isValidObjectId(videoId)){
+      throw new APIError(400,"This video doesn't exists")
+    }
+
     if (!content) {
       throw new APIError(404, "commment is required");
     }
-
     const comment = await Comment.create({
       content,
       video: videoId,
-      user: req.user_id,
+      owner: req.user._id,
     });
 
     if (!comment) {
@@ -41,16 +40,62 @@ const addComment = asyncHandler(async (req, res) => {
       .status(200)
       .json(new ApiResponse(200, comment, "Comment created successfully"));
   } catch (error) {
-    throw new APIError(400,error);
+    throw new APIError(400, error);
   }
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  try {
+    if (!isValidObjectId(commentId)) {
+      throw new APIError(404, "Comment Does not exists");
+    }
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      throw new APIError(400, "comment doesn't exists");
+    }
+    if (comment.owner.toString() != req.user._id.toString()) {
+      throw new APIError(400, " you are not allowed to change this comment");
+    }
+    const newComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        $set: { content },
+      },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json(new ApiResponse(200, newComment, "Comment updated sucessfully "));
+  } catch (error) {
+    throw new APIError(500, "Something went wrong ");
+  }
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+  try {
+    if (!isValidObjectId(commentId)) {
+      throw new APIError(400, "Invalid Comment id ");
+    }
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      throw new APIError(400, "comment doesn't exists");
+    }
+
+    if (comment.owner.toString() != req.user._id.toString()) {
+      throw new APIError(400, "you are not allowed to delete this comment");
+    }
+    await Comment.findByIdAndDelete(commentId);
+
+    res.status(200).json(new ApiResponse(200, "Comment deleted successfully"));
+  } catch (error) {
+    throw new APIError(500, error, "something went wrong ");
+  }
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
