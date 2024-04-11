ee9b2728c180cafed67e0dec584de8cee2601a89
devansh-kc <devansh.kc96@gmail.com>
2024-04-11 14:23:48 +0530

    made changes on cloudinary file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import { APIError } from "./apiError.js";
-
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
 
 cloudinary.config({
-  cloud_name: "dcjh2tkr8",
-  api_key: "537454131721289",
-  api_secret: "0VKaZAwG0A5ZRq1eueyqrQ4gtgE",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 // console.log(cloudinary.api);
 const uploadOnCloudinary = async (localFilePath) => {
